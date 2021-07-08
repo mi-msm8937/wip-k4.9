ed02b1c1c3709a3a2b7a0e37ab2ee1ae67bea624
zqm64 <zqm64@outlook.com>
2021-07-08 23:55:18 +0800

    [fixup] qti-haptics: do not allow dummy regulator

--- drivers/input/misc/qti-haptics.c
+++ drivers/input/misc/qti-haptics.c
@@ -1332,7 +1332,7 @@ static int qti_haptics_parse_dt(struct qti_hap_chip *chip)
 	if (config->act_type == ACT_REGULATOR) {
 		rc = of_property_read_string(node, "qcom,actuator-regulator-name", &str);
 		if (!rc) {
-			chip->vib_regulator = devm_regulator_get(chip->dev, str);
+			chip->vib_regulator = devm_regulator_get_optional(chip->dev, str);
 			if (IS_ERR(chip->vib_regulator)) {
 				dev_err(chip->dev, "Failed to get regulator actuator, deferring probe");
 				rc = -EPROBE_DEFER;
